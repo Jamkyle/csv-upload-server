78a8c174340146f1c463a2ab9d38f155d2af9a6c
Aro Rahari <raha.aro@gmail.com>
2024-10-28 14:56:45 +0100

    (test) fix csvFileController POST /upload

--- src/controllers/csvFileController.test.ts
+++ src/controllers/csvFileController.test.ts
@@ -8,14 +8,15 @@ const upload = multer({ storage: multer.memoryStorage() });
 app.post('/upload', upload.single('file'), csvFileController);
 
 describe('Controller', () => {
-    describe('csvController', () => {
+
+    describe('POST /upload', () => {
         it('should return 400 if no file is uploaded', async () => {
             const response = await request(app).post('/upload');
             expect(response.status).toBe(400);
             expect(response.body.error).toBe('No file uploaded.');
         });
 
-        it('should handle males and females correctly and return a zip file', async () => {
+        it('should return a ZIP file on successful upload', async () => {
             const mockCSVContent = `gender,name\nmale,John Doe\nfemale,Jane Doe`;
             const mockFileBuffer = Buffer.from(mockCSVContent);
 
@@ -24,13 +25,33 @@ describe('Controller', () => {
                 .attach('file', mockFileBuffer, {
                     filename: 'test.csv',
                     contentType: 'text/csv'
+                }).buffer()
+                .parse((res, callback) => {
+                    res.setEncoding('binary');
+                    res.on('data', (chunk) => {
+                        callback(null, Buffer.from(chunk, 'binary')); // Convert to Buffer
+                    });
                 });
-
             expect(response.status).toBe(200);
             expect(response.headers['content-type']).toBe('application/zip');
 
-            const buffer = response.body; // This should be a Buffer for the ZIP
-            expect(buffer).toBeInstanceOf(Buffer); // Ensure the response is a Buffer
+            expect(response.body).toBeInstanceOf(Buffer);
+        });
+
+        it('should return 500 if CSV files are empty', async () => {
+            // Simulate an upload where the processing results in empty CSV files
+            const mockCSVContent = `gender,name\n`;
+            const mockFileBuffer = Buffer.from(mockCSVContent);
+
+            const response = await request(app)
+                .post('/upload')
+                .attach('file', mockFileBuffer, {
+                    filename: 'empty.csv',
+                    contentType: 'text/csv'
+                })
+
+            expect(response.status).toBe(500);
+            expect(response.body.error).toBe('CSV files are empty.');
         });
     });
 
